0c20176d45a1ca801a609adcdc8ce2d78ffa9306
Krzysztof Polak <kriss145@gmail.com>
2025-12-29 22:10:38 +0100

    improve: update mock data

--- emails-previews/contact-form.tsx
+++ emails-previews/contact-form.tsx
@@ -2,10 +2,12 @@ import { defaultTheme } from "../src/templates/shared/theme";
 import { renderTemplateSync } from "../src/templates/emails";
 
 export const contactFormMockData: any = {
-  name: "Test Name",
-  email: "test@test.com",
-  phone: "1234567890",
-  message: "Test messages",
+  contact_form: {
+    name: "Test Name",
+    email: "test@test.com",
+    phone: "1234567890",
+    message: "Test messages",
+  }
 };
 
 export default function ContactForm() {
--- emails-previews/order-completed.tsx
+++ emails-previews/order-completed.tsx
@@ -2,39 +2,70 @@ import { defaultTheme } from "../src/templates/shared/theme";
 import { renderTemplateSync } from "../src/templates/emails";
 
 export const orderCompletedMockData: any = {
-  sales_channel: {
-    name: "Test Sales Channel",
-    description: "Test Sales Channel Description"
-  },
-  orderNumber: "1234567890",
-  customerName: "Jan Kowalski",
-  customerEmail: "jan@example.com",
-  orderDate: "2021-01-01",
-  completedDate: "2021-01-05",
-  totalAmount: "100.00",
-  currency_code: "pln",
-  items: [
-    {
-      thumbnail: "",
-      title: "Test Product 1",
-      quantity: 10,
-      price: "50.00"
+  order: {
+    sales_channel: {
+      name: "Test Sales Channel",
+      description: "Test Sales Channel Description"
     },
-    {
-      thumbnail: "https://placehold.co/150",
-      title: "Test Product 2",
-      quantity: 1,
-      price: "100.00"
+    orderNumber: "1234567890",
+    customerName: "Jan Kowalski",
+    customerEmail: "jan@example.com",
+    orderDate: "2021-01-01",
+    completedDate: "2021-01-05",
+    totalAmount: "100.00",
+    currency_code: "pln",
+    items: [
+      {
+        thumbnail: "",
+        title: "Test Product 1",
+        quantity: 10,
+        price: "50.00"
+      },
+      {
+        thumbnail: "https://placehold.co/150",
+        title: "Test Product 2",
+        quantity: 1,
+        price: "100.00"
+      }
+    ],
+    shippingAddress: "Test Shipping Address <br />Test Shipping Address 2",
+    billingAddress: "Test Billing Address<br />Test Billing Address 2",
+    orderUrl: "https://example.com/order/1234567890",
+    summary: {
+      total: "100.00",
+      paid_total: "100.00",
+      tax_total: "10.00",
+      discount_total: "0.00"
+    },
+    transformed: {
+      order_number: "1234567890",
+      order_date: "2021-01-01",
+      completed_date: "2021-01-05",
+      total_amount: "100.00",
+      currency_code: "pln",
+      items: [
+        {
+          thumbnail: "https://placehold.co/150",
+          title: "Test Product 1",
+          quantity: 10,
+          price: "50.00"
+        },
+        {
+          thumbnail: "https://placehold.co/150",
+          title: "Test Product 2",
+          quantity: 1,
+          price: "100.00"
+        }
+      ],
+      shipping_address_text: "Test Shipping Address <br />Test Shipping Address 2",
+      billing_address_text: "Test Billing Address<br />Test Billing Address 2",
+      summary: {
+        total: "100.00",
+        paid_total: "100.00",
+        tax_total: "10.00",
+        discount_total: "0.00"
+      }
     }
-  ],
-  shippingAddress: "Test Shipping Address <br />Test Shipping Address 2",
-  billingAddress: "Test Billing Address<br />Test Billing Address 2",
-  orderUrl: "https://example.com/order/1234567890",
-  summary: {
-    total: "100.00",
-    paid_total: "100.00",
-    tax_total: "10.00",
-    discount_total: "0.00"
   }
 };
 
