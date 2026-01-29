8e9c8f5fa837007128c892ac868e5128706375d3
Krzysztof Polak <kriss145@gmail.com>
2026-01-29 21:00:19 +0100

    improve: two columns in blocks view

--- src/admin/routes/mpn/templates/[id]/blocks/page.tsx
+++ src/admin/routes/mpn/templates/[id]/blocks/page.tsx
@@ -2,6 +2,7 @@ import { defineRouteConfig } from "@medusajs/admin-sdk"
 import { SingleColumnPage } from "../../../../../components/layout/pages"
 import { BlocksContainer } from "../../../../../builder/blocks"
 import { useParams } from "react-router-dom"
+import { TwoColumnPage } from "../../../../../components/layout/pages/two-column-page/two-column-page"
 
 const ListPage = () => {
   const { id } = useParams()
@@ -13,7 +14,14 @@ const ListPage = () => {
         after: [],
       }}
     >
-      <BlocksContainer id={id ?? ""} />
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+        <div className="col-span-1">
+          <BlocksContainer id={id ?? ""} />
+        </div>
+        <div className="col-span-1">
+          View email preview
+        </div>
+      </div>
     </SingleColumnPage>
   )
 }
