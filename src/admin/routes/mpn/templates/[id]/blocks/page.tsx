299c4b84a7e7ea019e5ed835b38cb84ebf509d5d
Krzysztof Polak <kriss145@gmail.com>
2026-03-13 18:37:17 +0100

    feat: add preview toggle functionality to Blocks page for enhanced user experience

--- src/admin/routes/mpn/templates/[id]/blocks/page.tsx
+++ src/admin/routes/mpn/templates/[id]/blocks/page.tsx
@@ -1,4 +1,7 @@
 import { defineRouteConfig } from "@medusajs/admin-sdk"
+import { ListBullet } from "@medusajs/icons"
+import { Button, Container, Heading } from "@medusajs/ui"
+import { useState } from "react"
 import { useParams } from "react-router-dom"
 import { SingleColumnPage } from "../../../../../components/layout/pages"
 import { BlocksContainer } from "../../../../../builder/blocks"
@@ -6,6 +9,8 @@ import { BlocksPreviewContainer } from "../../../../../builder/blocks/blocks-pre
 
 const ListPage = () => {
   const { id } = useParams()
+  const [isPreviewVisible, setIsPreviewVisible] =
+    useState(false)
 
   return (
     <SingleColumnPage
@@ -14,13 +19,35 @@ const ListPage = () => {
         after: [],
       }}
     >
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+      <Container>
+        <div className="flex justify-between">
+          <Heading level="h1">Blocks</Heading>
+          <Button
+            variant="secondary"
+            size="small"
+            onClick={() =>
+              setIsPreviewVisible((prev) => !prev)
+            }
+          >
+            {isPreviewVisible
+              ? "Hide preview"
+              : "Show preview"}
+          </Button>
+        </div>
+      </Container>
+      <div
+        className={`grid grid-cols-1 gap-4 ${
+          isPreviewVisible ? "md:grid-cols-2" : ""
+        }`}
+      >
         <div className="col-span-1">
           <BlocksContainer id={id ?? ""} />
         </div>
-        <div className="col-span-1">
-          <BlocksPreviewContainer templateId={id ?? ""} />
-        </div>
+        {isPreviewVisible && (
+          <div className="col-span-1">
+            <BlocksPreviewContainer templateId={id ?? ""} />
+          </div>
+        )}
       </div>
     </SingleColumnPage>
   )
